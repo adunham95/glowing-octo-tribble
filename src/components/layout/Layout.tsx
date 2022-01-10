bf3d762c49c15ae937cc49c905de729d4decd5ae
adunham95 <adunham95@gmail.com>
2022-01-10 16:28:48 -0500

    Added layout

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { DarkMode } from '../Utilities/DarkMode'
+
+interface IProps{
+    children: React.ReactChild,
+    useHeader: boolean
+}
+
+const Layout = ({children}: IProps) => {
+    return (
+        <div className='container mx-auto'>
+            <DarkMode/>
+            <div className=''>
+            {children}
+            </div>
+        </div>
+    )
+}
+
+export default Layout
