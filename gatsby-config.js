992ea56e4b093ccd5957deda0a502ed1f52ef020
adunham95 <adunham95@gmail.com>
2022-01-10 16:12:06 -0500

    Intial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,18 @@ module.exports = {
     title: "Personal Website",
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+    
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    'gatsby-plugin-postcss',
     {
       resolve: "gatsby-plugin-manifest",
       options: {
