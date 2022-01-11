b052790a6d5ce36317f9d7011f38484b82cca77a
adunham95 <adunham95@gmail.com>
2022-01-11 10:53:00 -0500

    Removed console messages

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,14 +2,11 @@ const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
-  console.log(node.internal.type);
 
   //   fmImagesToRelative(node);
 
   if (node.internal.type === `Mdx`) {
-    console.log(node.fields); // console.log(node);
     const value = createFilePath({ node, getNode });
-    console.log(value);
     createNodeField({
       name: `slug`,
       node,
