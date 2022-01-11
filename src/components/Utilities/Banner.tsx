1edd4cc848ddfe23c9a123c336cf5b04a641171c
adunham95 <adunham95@gmail.com>
2022-01-11 07:35:04 -0500

    Added banner

---
+++ src/components/Utilities/Banner.tsx
@@ -0,0 +1,170 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline';
+
+interface IStyle {
+  background: string;
+  iconColor: string;
+  text?: string;
+  button: string;
+  close?: string;
+}
+
+interface IStyles {
+  info: IStyle;
+  error: IStyle;
+  warn: IStyle;
+  success: IStyle;
+}
+
+export enum EBannerStyleType {
+  WARN = 'warn',
+  ERROR = 'error',
+  INFO = 'info',
+  SUCCESS = 'success',
+  CUSTOM = 'custom',
+}
+
+interface IProps {
+  id: string;
+  bannerType?: EBannerStyleType;
+  link?: string;
+  linkText?: string;
+  copy: string;
+  customStyles?: IStyle;
+}
+
+const styles: IStyles = {
+  info: {
+    background: 'bg-indigo-600',
+    iconColor: 'bg-indigo-800',
+    text: 'text-white',
+    button: 'text-indigo-600 bg-white hover:bg-indigo-50',
+    close: 'hover:bg-indigo-500 focus:ring-white',
+  },
+  error: {
+    background: 'bg-red-600',
+    iconColor: 'bg-red-800',
+    text: 'text-white',
+    button: 'text-red-600 bg-white hover:bg-red-50',
+    close: 'hover:bg-red-500',
+  },
+  warn: {
+    background: 'bg-amber-600',
+    iconColor: 'bg-amber-800',
+    text: 'text-white',
+    button: 'text-amber-600 bg-white hover:bg-amber-50',
+    close: 'hover:bg-yellow-500',
+  },
+  success: {
+    background: 'bg-green-600',
+    iconColor: 'bg-green-800',
+    text: 'text-white',
+    button: 'text-green-600 bg-white hover:bg-green-50',
+    close: 'hover:bg-green-500',
+  },
+};
+
+const Banner = ({
+  id,
+  bannerType,
+  link = '',
+  linkText = 'Learn More',
+  customStyles,
+  copy,
+}: IProps) => {
+  const [bannerIds, setBannerIds] = useState([]);
+  const [showBanner, setShowBanner] = useState(false);
+
+  useLayoutEffect(() => {
+    const banners = localStorage.getItem('bannerIds');
+    if (banners) {
+      const selectedBanners = JSON.parse(banners);
+      setBannerIds(selectedBanners);
+      if (!selectedBanners.includes(id)) {
+        setShowBanner(true);
+      }
+      if (id === '') setShowBanner(false);
+    }
+  }, []);
+
+  const closeBanner = () => {
+    setShowBanner(false);
+    localStorage.setItem('bannerIds', JSON.stringify([...bannerIds, id]));
+  };
+
+  const getStyles = (type: EBannerStyleType) => {
+    switch (type) {
+      case EBannerStyleType.WARN:
+        return styles.warn;
+      case EBannerStyleType.ERROR:
+        return styles.error;
+      case EBannerStyleType.SUCCESS:
+        return styles.success;
+      case EBannerStyleType.CUSTOM:
+        return { ...styles.info, ...customStyles };
+      default:
+        return styles.info;
+    }
+  };
+
+  if (!showBanner) {
+    return <></>;
+  }
+
+  return (
+    <div className={getStyles(bannerType).background}>
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="w-0 flex-1 flex items-center">
+            <span
+              className={`flex p-2 rounded-lg ${
+                getStyles(bannerType).iconColor
+              }`}
+            >
+              <SpeakerphoneIcon
+                className={`h-6 w-6 ${getStyles(bannerType).text}`}
+                aria-hidden="true"
+              />
+            </span>
+            <p
+              className={`ml-3 font-medium truncate ${
+                getStyles(bannerType).text
+              }`}
+            >
+              <span className="md:inline">{copy}</span>
+            </p>
+          </div>
+          {link !== '' && (
+            <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+              <a
+                href="#"
+                className={`flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium ${
+                  getStyles(bannerType).button
+                }`}
+              >
+                {linkText}
+              </a>
+            </div>
+          )}
+          <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+            <button
+              type="button"
+              className={`-mr-1 flex p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2 ${
+                getStyles(bannerType).close
+              }`}
+              onClick={closeBanner}
+            >
+              <span className="sr-only">Dismiss</span>
+              <XIcon
+                className={`h-6 w-6 ${getStyles(bannerType).text}`}
+                aria-hidden="true"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,33 @@
-import React from "react";
-import { DarkMode } from "../Utilities/DarkMode";
+import React from 'react';
+import Banner, { EBannerStyleType } from '../Utilities/Banner';
+import { DarkMode } from '../Utilities/DarkMode';
 
 interface IProps {
   children: React.ReactChild;
   useHeader: boolean;
 }
 
+const bannerData = {
+  id: '',
+  link: '',
+  linkText: '',
+  copy: 'Banner Data',
+  bannerType: EBannerStyleType.CUSTOM,
+  customStyles: {
+    background: 'bg-fuchsia-600',
+    iconColor: 'bg-fuchsia-800',
+    text: 'text-white',
+    button: 'text-fuchsia-600 bg-white hover:bg-fuchsia-50',
+    close: 'hover:bg-fuchsia-500 focus:ring-white',
+  },
+};
+
 const Layout = ({ children }: IProps) => {
   return (
-    <div className="container mx-auto">
+    <div className="">
+      <Banner {...bannerData} />
       <DarkMode />
-      <div className="">{children}</div>
+      <div className="container mx-auto">{children}</div>
     </div>
   );
 };
