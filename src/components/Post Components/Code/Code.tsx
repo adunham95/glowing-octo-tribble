efe325da332465afe118ac568f9eb861b177fe51
adunham95 <adunham95@gmail.com>
2022-01-12 13:57:43 -0500

    Added custom theme

--- src/components/Post Components/Code/Code.tsx
+++ src/components/Post Components/Code/Code.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable*/
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/vsDark';
+import vsCodeTheme from './themes/vsCodeTheme';
 
 export default (props) => {
   const className = props.children.props.className || '';
@@ -16,7 +16,7 @@ export default (props) => {
           ? matches.groups.lang
           : ''
       }
-      theme={theme}
+      theme={vsCodeTheme}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={{ ...style, padding: '20px' }}>
