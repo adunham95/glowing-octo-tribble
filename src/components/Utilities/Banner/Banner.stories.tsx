3f22ecd36b4a094e469a9df8d07e87d1c4614736
adunham95 <adunham95@gmail.com>
2022-01-12 11:20:55 -0500

    Cleaned up

--- src/components/Utilities/Banner/Banner.stories.tsx
+++ src/components/Utilities/Banner/Banner.stories.tsx
@@ -3,21 +3,15 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import Banner, { EBannerStyleType } from './Banner';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Layout/Banner',
   component: Banner,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-      id: 'test',
-  },
+// eslint-disable-next-line prettier/prettier
 } as ComponentMeta<typeof Banner>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Banner> = (args) => <Banner {...args}/>
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
     bannerType: EBannerStyleType.INFO,
     id: 'test',
