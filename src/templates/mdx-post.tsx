c5e129e1a5eaaeeea21a909fbb8110e448ef766f
adunham95 <adunham95@gmail.com>
2022-01-12 13:54:11 -0500

    Moved render

--- src/templates/mdx-post.tsx
+++ src/templates/mdx-post.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import MdxLayout from '../components/layout/MDX_Layout';
 import PostTitle from '../components/Post Components/PostTitle/PostTitle';
+import Pill, { EPillType } from '../components/Post Components/Pill';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 const MDXPostTemplate = ({ data, pageContext, location }) => {
   const post = data.mdx;
   return (
     <MdxLayout>
       <PostTitle title={post.frontmatter.title} />
-      {post.body}
+      <div>
+        <Pill pillType={EPillType.INFO} text="Test" />
+      </div>
+      <MDXRenderer>{post.body}</MDXRenderer>
     </MdxLayout>
   );
 };
