158e7d3a6aa8dec5e75a0256587271e77e6fc118
adunham95 <adunham95@gmail.com>
2022-01-12 09:22:25 -0500

    Removed images

--- src/components/Post Components/SEO.tsx
+++ src/components/Post Components/SEO.tsx
@@ -45,7 +45,7 @@ const SEO = ({ title, description, image, article = false }: IProps) => {
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      {/* <meta name="image" content={seo.image} /> */}
       {seo.url && <meta property="og:url" content={seo.url} />}
       {(article ? true : null) && <meta property="og:type" content="article" />}
       {seo.title && <meta property="og:title" content={seo.title} />}
