286c8520f9430092ea6fa1378ebf55b892034eb5
adunham95 <adunham95@gmail.com>
2022-01-14 08:09:31 -0500

    Updated brand colors

--- src/components/Utilities/Footer/index.tsx
+++ src/components/Utilities/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <div className="flex justify-between p-1 text-sm bg-slate-400 text-white h-[30px]">
+    <div className="flex justify-between p-1 text-sm bg-brand-dark-400 text-brand-light-500 h-[30px]">
       <div>&copy; Copyright {year}</div>
       <div>Adrian Dunham</div>
     </div>
